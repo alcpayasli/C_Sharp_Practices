01f9cfb881d08622dc41fc3a8a6f5746f9090bbf
Ali Can Payasli <84179271+alcpayasli@users.noreply.github.com>
2024-11-07 15:41:25 +0300

    Add files via upload

---
+++ Account1/AccountTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class AccountTest
+{
+    static void Main()
+    {
+        Account myAccount = new Account();
+        Console.WriteLine($"Inital name: {myAccount.GetName()}");
+        Console.Write("Enter the name:");
+        string theName = Console.ReadLine();
+        myAccount.SetName(theName);
+        Console.WriteLine($"Name of account: {myAccount.GetName()}");
+    }
+}
---
+++ Account2/AccountTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class AccountTest
+{
+    static void Main()
+    {
+        Account myAccount = new Account();
+        Console.Write("Enter the name:");
+        string theName = Console.ReadLine();
+        myAccount.Name = theName;
+        Console.WriteLine($"Account name: {myAccount.Name}");
+    }
+}
