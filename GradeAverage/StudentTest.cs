ab4ae795a5a5021fcc83d3eea81a9e1f99171a52
Ali Can Payasli <84179271+alcpayasli@users.noreply.github.com>
2024-12-03 11:00:37 +0300

    Add files via upload

---
+++ GradeAverage/StudentTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class StudentTest
+{
+    static void Main()
+    {
+        Student std1 = new Student("James Green", 92);
+        Student std2 = new Student("Ashley Cole", 45);
+        Student std3 = new Student("Janner Mans", 77);
+        Console.WriteLine(
+            $"{std1.Name}'s average {std1.Average} and lettergrade {std1.LetterGrade}");
+        Console.WriteLine(
+            $"{std2.Name}'s average {std2.Average} and lettergrade {std2.LetterGrade}");
+        Console.WriteLine(
+            $"{std3.Name}'s average {std3.Average} and lettergrade {std3.LetterGrade}");
+
+    }
+}
---
+++ StaticEmployee/EmployeeTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class EmployeeTest
+{
+    static void Main()
+    {
+        Console.WriteLine(
+            $"Employees before instantiation: {Employee.Count}");
+
+        var e1 = new Employee("Susan", "Baker");
+        var e2 = new Employee("Bob", "Blue");
+        Console.WriteLine(
+        $"Employees after instantiation: {Employee.Count}");
+    }
+}
